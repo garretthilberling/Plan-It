e52a1eabc548bc660c570f1ed4faaa560f289e0f
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-06 22:15:56 -0400

    changes to landing page styling

--- client/src/components/GetStarted/index.jsx
+++ client/src/components/GetStarted/index.jsx
@@ -10,18 +10,18 @@ const GetStarted = () => {
   return (
     <>
     <div className='text-center text-2xl py-40'>
-    <p className='landing-text text-6xl'>Save Your Plan-It and Get Organized!</p>
+    <p className='landing-text text-6xl text-cream-200'>Login or Create An Account to Get Planning</p>
     <div className='pt-3'>
       
       <Link to="/login">
-      <button className="hover:bg-bubblegum-500 pt-3 mx-2 pb-3 pl-12 pr-12 text-2xl ring-4 ring-bubblegum-500 font-semibold text-center text-white transition-all rounded-full lg:ml-5  focus:outline-none  lg:font-medium">
+      <button className="bg-gradient-to-t from-bubblegum-500 hover:bg-bubblegum-400 pt-3 mx-2 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all ease-in-out duration-500 rounded-lg lg:ml-5  focus:outline-none lg:font-medium">
         Login
         </button>
         </Link>
 
         <Link to="/signup">
       <button 
-      className='hover:bg-bubblegum-500 pt-3 pb-3 mx-2 ring-4 ring-bubblegum-500 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all  rounded-full  lg:ml-5  focus:outline-none  lg:font-medium'>
+      className="bg-gradient-to-t from-bubblegum-500 hover:bg-bubblegum-400 pt-3 mx-2 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all ease-in-out duration-500 rounded-lg lg:ml-5  focus:outline-none lg:font-medium">
         Sign Up
         </button>
         </Link>
